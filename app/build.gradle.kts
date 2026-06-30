37e4e0c9d794c8bfdd4121e85a68487438968148
gtlx <gtlx2233@qq.com>
2026-06-30 18:35:18 +0800

    fix: compose-compiler 改名避免版本目录冲突

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,7 +45,7 @@ android {
     }
 
     composeOptions {
-        kotlinCompilerExtensionVersion = libs.versions.compose.compiler.get()
+        kotlinCompilerExtensionVersion = libs.versions.composeKotlinCompiler.get()
     }
 }
 
